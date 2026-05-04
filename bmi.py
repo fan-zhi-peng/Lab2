36aba8ab6b2cfd274f6645618813f4e066d8a963
ZhiPeng <FZHIPENG09.25@ICHAT.SP.EDU.SG>
2026-05-04 14:44:22 +0800

    bmi2

--- bmi.py
+++ bmi.py
@@ -5,8 +5,11 @@ def calculate_bmi(height,weight):
     print("bmi: " +str(bmi))
     if bmi <18.5:
         print("You are underweight")
+        return -1
     if bmi > 25.0:
         print("You are overweight")
+        return 1
     else:
         print("You are normal weight")
+        return 0
 calculate_bmi(weight=57.0, height=1.73)
